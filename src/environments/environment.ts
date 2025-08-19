f4059b045df7e613c8e2fcc01f40bbfa97b21465
Edis MR <edisanthony@gmail.com>
2025-08-19 13:27:13 -0600

    build style correction

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	shortNameVersion: '250819-4',
+	shortNameVersion: '250819-5',
 	dateBuild: new Date().toISOString(),
 	baseUrl: "https://edismr.github.io/songs-visor/",
 	songsFile: "SongsDB-20250819-124852.json"
