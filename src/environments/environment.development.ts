b55745269abf6dd9a36af01a77def12e025a32c6
Edis MR <edisanthony@gmail.com>
2025-08-19 12:56:50 -0600

    build importation correction

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: false,
-	shortNameVersion: '250819-2',
+	shortNameVersion: '250819-3',
 	dateBuild: new Date().toISOString(),
 	baseUrl: 'http://192.168.4.109:4200/',
 	songsFile: "SongsDB-20250819-124852.json"
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	shortNameVersion: '250819-2',
+	shortNameVersion: '250819-3',
 	dateBuild: new Date().toISOString(),
 	baseUrl: "https://edismr.github.io/songs-visor/",
 	songsFile: "SongsDB-20250819-124852.json"
