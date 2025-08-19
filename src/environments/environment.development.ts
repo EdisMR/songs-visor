2831abc880345c7ab90f4f72ed65bd8b00842043
Edis MR <edisanthony@gmail.com>
2025-08-19 12:12:40 -0600

    build auto import

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-	shortNameVersion: '250818-3',
+	shortNameVersion: '250819-1',
 	dateBuild: new Date().toISOString(),
 	baseUrl:'http://192.168.4.109:4200/',
 	hashSongsFile:"5DD3BC7C85C481BACCF280C0BA23D66A",
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-	shortNameVersion: '250818-3',
+	shortNameVersion: '250819-1',
 	dateBuild: new Date().toISOString(),
 	baseUrl:"https://edismr.github.io/songs-visor/",
 	hashSongsFile:"5DD3BC7C85C481BACCF280C0BA23D66A",
