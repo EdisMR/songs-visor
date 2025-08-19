3d4f0f2186e2db8d7349608fc44da44ac8a167ba
Edis MR <edisanthony@gmail.com>
2025-08-19 12:22:52 -0600

    environment use correction

--- src/app/services/common.service.ts
+++ src/app/services/common.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 import { CategoryInterface } from '../interface/category-interface';
 import { JsonFileInterface } from '../interface/json-file-interface';
 import { SongInterface } from '../interface/song-interface';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
-	shortNameVersion: '250819-1',
+	production: true,
+	shortNameVersion: '250819-2',
 	dateBuild: new Date().toISOString(),
-	baseUrl:"https://edismr.github.io/songs-visor/",
-	hashSongsFile:"5DD3BC7C85C481BACCF280C0BA23D66A",
-	songsFile:"SongsDB-20250817-085414.json"
+	baseUrl: "https://edismr.github.io/songs-visor/",
+	hashSongsFile: "5DD3BC7C85C481BACCF280C0BA23D66A",
+	songsFile: "SongsDB-20250817-085414.json"
 };
