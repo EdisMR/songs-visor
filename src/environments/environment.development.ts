4da42f6a388c440f96a8191ee2b0501efc0a5d1a
Edis MR <edisanthony@gmail.com>
2025-08-29 19:26:43 -0600

    new icon and songs added

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: false,
-	shortNameVersion: '250829-2',
+	shortNameVersion: '250829-3',
 	dateBuild: new Date().toISOString(),
 	baseUrl: 'http://192.168.4.109:4200/',
-	songsFile: "SongsDB-20250829-183111.json"
+	songsFile: "SongsDB-20250829-191707.json"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: true,
-	shortNameVersion: '250829-2',
+	shortNameVersion: '250829-3',
 	dateBuild: new Date().toISOString(),
 	baseUrl: "https://edismr.github.io/songs-visor/",
-	songsFile: "SongsDB-20250829-183111.json"
+	songsFile: "SongsDB-20250829-191707.json"
 };
