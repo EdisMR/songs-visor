13770ecdf52afc650e37aedfb8af718b4a1e6fff
Edis MR <edisanthony@gmail.com>
2025-08-29 18:11:06 -0600

    build alertify implemented

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: false,
-	shortNameVersion: '250827-1',
+	shortNameVersion: '250829-1',
 	dateBuild: new Date().toISOString(),
 	baseUrl: 'http://192.168.4.109:4200/',
 	songsFile: "SongsDB-20250827-155756.json"
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	shortNameVersion: '250827-1',
+	shortNameVersion: '250829-1',
 	dateBuild: new Date().toISOString(),
 	baseUrl: "https://edismr.github.io/songs-visor/",
 	songsFile: "SongsDB-20250827-155756.json"
